80ab7376731e4bf81cc1cf6d22ecc42404aadb2f
Miidoriya <99621562+Miidoriya@users.noreply.github.com>
2023-02-17 17:20:20 +0000

    WIP: Retrieving single comic issue data

--- basic-lambda/src/main.rs
+++ basic-lambda/src/main.rs
@@ -1,5 +1,17 @@
+use std::sync::Arc;
 use lambda_runtime::{service_fn, LambdaEvent, Error};
 use serde_json::{json, Value};
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize)]
+struct ComicInfo {
+    name: String,
+    writer: String,
+    artist: String,
+    publisher: String,
+    release_date: String,
+    cover_price: String,
+}
 
 const URL: &str = "https://comicbookroundup.com/comic-books/reviews/marvel-comics/immortal-x-men-(2022)/8";
 
@@ -11,16 +23,25 @@ async fn main() -> Result<(), Error> {
 }
 
 async fn func(event: LambdaEvent<Value>) -> Result<Value, Error> {
-    let (event, _context) = event.into_parts();
+    let (_event, _context) = event.into_parts();
     let response = get_response(URL).await?;
-    let name = parse_name(&response);
-    let name = match name {
-        Ok(name) => name,
-        Err(_) => "Some error retrieving name".to_string(),
+    let name = parse_name(&response).unwrap();
+    let writer = parse_comic_info_field(&response, "Writer").unwrap();
+    let artist = parse_comic_info_field(&response, "Artist").unwrap();
+    let publisher = parse_comic_info_field(&response, "Publisher").unwrap();
+    let release_date = parse_comic_info_field(&response, "Release Date").unwrap();
+    let cover_price = parse_comic_info_field(&response, "Cover Price").unwrap();
+    let comic_info = ComicInfo {
+        name,
+        writer,
+        artist,
+        publisher,
+        release_date,
+        cover_price,
     };
     Ok(json!({
         "statusCode": 200,
-        "body": name
+        "body": comic_info,
     }))
 }
 
@@ -33,13 +54,24 @@ async fn get_response(url: &str) -> Result<String, reqwest::Error>{
 }
 
 fn parse_name(response: &str) -> Result<String, scraper::error::SelectorErrorKind> {
-    let response = scraper::Html::parse_document(&response);
+    let response = scraper::Html::parse_document(response);
     let name_selector =
         scraper::Selector::parse("div.series-buttons a.series")?;
     let name = match response.select(&name_selector).next() {
-        Some(name) => name.value().attr("href").unwrap().to_string(),
+        Some(name) => name.value().attr("href").unwrap().to_string().split('/').last().unwrap().to_string(),
         None => "N/A".to_string(),
     };
     Ok(name)
+}
+
+fn parse_comic_info_field<'a>(response: &'a str, field: &'a str) -> Result<String, scraper::error::SelectorErrorKind<'a>> {
+    let response = scraper::Html::parse_document(response);
+    let name_selector =
+        scraper::Selector::parse(".issue div.container div.right div.left span")?;
+    let field = match response.select(&name_selector).find(|n| Arc::new(n.text().collect::<Vec<_>>().join("")).contains(field)) {
+        Some(name) => name.text().collect::<Vec<_>>().last().unwrap().to_string(),
+        None => "N/A".to_string(),
+    };
+    Ok(field)
 }
 
